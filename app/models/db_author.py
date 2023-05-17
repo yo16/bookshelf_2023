218865f9a2831ed7a5529ef2dd31c9ee6193c1bc
yo16 <xxyo1xx@gmail.com>
2023-05-17 21:38:14 +0900

    add get_new_book_id()

--- app/models/db_author.py
+++ app/models/db_author.py
@@ -1,7 +1,8 @@
-from sqlalchemy import String
+from sqlalchemy import String, select
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.expression import func
 
-from .db_common import Base
+from .db_common import Base, get_db
 
 
 class DbAuthor(Base):
@@ -9,3 +10,14 @@ class DbAuthor(Base):
 
     author_id: Mapped[int] = mapped_column(primary_key=True)
     author_name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    @staticmethod
+    def get_new_book_id():
+        db = next(get_db())
+        result = db.execute(
+            select(
+                func.max(DbAuthor.author_id).label("max_author_id")
+            )
+        ).scalars().first()
+        
+        return result.max_author_id + 1
