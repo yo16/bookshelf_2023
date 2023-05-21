b5265d03460aae2b8ed83d2b47703c0e050002ee
yo16 <xxyo1xx@gmail.com>
2023-05-21 15:37:06 +0900

    本の登録のDB処理ができた

--- app/views/get_book_with_isbn_view.py
+++ app/views/get_book_with_isbn_view.py
@@ -23,16 +23,16 @@ def main(app):
         "genres": "to be implemented!!"
     }
 
-    print(10)
+    #print(10)
     # DBを探す
     db = next(get_db())
     cur_book = db.execute(
         select(DbBook).where(DbBook.isbn == isbn)
     ).scalars().first()
 
-    print(20)
+    #print(20)
     if cur_book:
-        print(100)
+        #print(100)
         # DBに見つかったので関連情報を取得
         ret_dic["book_name"] = cur_book.book_name
         ret_dic["publisher_id"] = cur_book.publisher_id
@@ -59,7 +59,7 @@ def main(app):
         ret_dic["publisher_code"] = publisher.publisher_code
 
     else:
-        print(200)
+        #print(200)
         # 見つからなかったので、Googleに問い合わせる
         url = f"https://www.googleapis.com/books/v1/volumes"
         params = {
@@ -69,13 +69,13 @@ def main(app):
         params_urlencoded = urllib.parse.urlencode(params)
         #params_urlencoded = params_urlencoded.encode("ascii")
         url_params = f"{url}?{params_urlencoded}"
-        print(f"210:{url_params}")
+        #print(f"210:{url_params}")
         
         #res = requests.get(url, params=params)
         res = requests.get(url_params)
-        print("211")
-        print(res)
-        print(res.text)
+        #print("211")
+        #print(res)
+        #print(res.text)
         res_data = json.loads(res.text)
 
         item = res_data["items"][0]
@@ -94,11 +94,11 @@ def main(app):
         ret_dic["publisher_code"] = DbPublisher.get_publisher_code_from_isbn(isbn)
         ret_dic["publisher_name"] = "to be implemented!"
 
-        print("999")
-        print(ret_dic)
+        #print("999")
+        #print(ret_dic)
 
     
     # ジャンル
 
-    print(1000)
+    #print(1000)
     return jsonify(ResultSet=json.dumps(ret_dic))
--- app/views/maintenance_view.py
+++ app/views/maintenance_view.py
@@ -2,7 +2,7 @@ from flask import render_template, request
 from flask_login import current_user
 from sqlalchemy import select
 
-from models import get_db, DbOrganization
+from models import get_db, DbBook, DbAuthor, DbWriting, DbPublisher
 from views.view_common import get_org_mem
 from views.forms import RegistBookForm
 
@@ -13,6 +13,153 @@ def main(app):
 
     if form.validate_on_submit():
         # 登録処理
-        print("----- validate OK")
+        regist_info()
 
     return render_template("maintenance.html", **org_mem, form=form)
+
+
+def regist_info():
+    """登録情報をもとに、関連テーブルを登録する
+    """
+    req = {
+        "isbn": request.form["isbn"],
+        "book_name": request.form["book_name"],
+        "image_url": request.form["image_url"],
+        "authors": [],
+        "num_of_authors": int(request.form["num_of_authors"]),
+        "publisher_code": request.form["publisher_code"],
+        "publisher_name": request.form["publisher_name"],
+        "comment": request.form["comment"],
+        "genres": request.form["genres"]
+    }
+    for i in range(req["num_of_authors"]):
+        author = ""
+        if f"author{i}" in request.form:
+            author = request.form[f"author{i}"]
+        req["authors"].append(author)
+    
+    # 登録する情報
+    book = create_book(req)
+    authors = create_authors(req)
+    writings = create_writings(req, book, authors)
+    publisher = create_publisher(req)
+    book.publisher_id = publisher.publisher_id
+
+    # 登録
+    db = next(get_db())
+    db.add(book)
+    for a in authors:
+        if a["is_new_author"]:
+            db.add(a["author"])
+    for w in writings:
+        db.add(w)
+    db.add(publisher)
+    db.commit()
+    db.refresh(book)
+    for a in authors:
+        if a["is_new_author"]:
+            db.refresh(a["author"])
+    for w in writings:
+        db.refresh(w)
+    
+    return
+    
+
+
+def create_book(info):
+    """登録するDbBookを作成
+
+    Args:
+        info (dict): requestから集めた情報
+    Returns
+    """
+    new_book_id = DbBook.get_new_book_id()
+    
+    return DbBook(
+        book_id = new_book_id,
+        isbn = info["isbn"],
+        book_name = info["book_name"],
+        image_url = info["image_url"],
+        publisher_id = None
+    )
+
+
+def create_authors(info):
+    """登録するDbAuthorを作成
+
+    Args:
+        info (dict): requestから集めた情報
+    """
+    ret = []
+    new_author_id = None
+    new_author_count = -1
+
+    for i, a in enumerate(info["authors"]):
+        is_new_author = False
+
+        # 登録済のDbAuthorがないか確認（名前で！）
+        db = next(get_db())
+        author = db.execute(
+            select(DbAuthor).where(DbAuthor.author_name == a)
+        ).scalar()
+        if not author:
+            # 未登録なので登録
+            is_new_author = True
+            if not new_author_id:
+                new_author_id = DbAuthor.get_new_author_id()
+            new_author_count += 1
+            author = DbAuthor(
+                author_id = new_author_id + new_author_count,
+                author_name = a
+            )
+        else:
+            # 登録されている
+            is_new_author = False
+            author = author.first()
+        
+        # 登録する/しないによらずappend
+        ret.append({
+            "author": author,
+            "is_new_author": is_new_author
+        })
+    
+    return ret
+
+
+def create_writings(info, book, authors):
+    """登録するDbWritingを作成
+
+    Args:
+        info (dict): requestから集めた情報
+        book (DbBook): 登録する本情報
+        authors (array[DbAuthor]): 登録する著者情報の配列
+    Returns:
+        array[DbWriting]
+    """
+    ret = []
+
+    for a in authors:
+        ret.append(DbWriting(
+            book_id = book.book_id,
+            author_id = a["author"].author_id
+        ))
+    
+    return ret
+
+
+def create_publisher(info):
+    """登録するDbPublisherを作成
+
+    Args:
+        info (dict): requestから集めた情報
+    """
+    new_pub_id = DbPublisher.get_new_publisher_id()
+    publisher_code = DbPublisher.get_publisher_code_from_isbn(info["isbn"])
+    publisher_name = f"code[{publisher_code}]"
+
+
+    return DbPublisher(
+        publisher_id = new_pub_id,
+        publisher_name = publisher_name,
+        publisher_code = publisher_code
+    )
