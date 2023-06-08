0913c73eaf572c36e882b7d7f2d906d669481cfe
yo16 <xxyo1xx@gmail.com>
2023-06-08 22:55:19 +0900

    collection.num_of_same_booksの更新ができていなかった

--- app/views/book_view.py
+++ app/views/book_view.py
@@ -1,8 +1,8 @@
-from flask_login import current_user
 from flask import render_template, request, redirect, url_for
-from sqlalchemy import select
+from sqlalchemy import update
+from sqlalchemy.sql.expression import and_
 
-from models import get_db, DbBook
+from models import get_db, DbBook, DbCollection
 from .view_common import get_org_mem
 from .forms import EditBookForm, BorrowBookForm, ReturnBookForm
 
@@ -16,6 +16,10 @@ def main(app):
     org_id = org_mem["organization"].org_id
     book_id = request.args.get("book_id")
     message = request.args.get("msg")
+
+    # 数の変更のための編集のPOSTがsubmitされた場合
+    if edit_form.is_submitted():
+        edit_book(org_id, book_id, int(request.form.get("num_of_same_books")))
 
     # 指定された本が組織に登録されているか確認しつつ本情報を取得
     book_info = DbBook.get_book_info(book_id, org_id)
@@ -34,4 +38,28 @@ def main(app):
         borrow_form=borrow_form,
         return_form=return_form
     )
+
+
+def edit_book(org_id, book_id, num_of_book):
+    """本の情報を変更する
+    現在は本の情報というか、collectionの本の数しかない。
+
+    Args:
+        org_id (int): org_id
+        book_id (int): book_id
+        num_of_book (int): 本の数
+    """
+    with get_db() as db:
+        stmt = update(
+            DbCollection
+        ).values(
+            num_of_same_books = num_of_book
+        ).where(
+            and_(
+                DbCollection.org_id == org_id,
+                DbCollection.book_id == book_id
+            )
+        )
+        db.execute(stmt)
+        db.commit()
 
